f6322b1c548d9ef640563714b73d5d46c80fe81f
Aedan Valle <valle.aedan@gmail.com>
2020-10-11 16:33:56 -0700

    changed some stuff

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,8 @@
     'use strict';
     var FORM_SELECTOR = '[data-coffee-order="form"]';
     var CHECKLIST_SELECTOR = '[data-coffee-order="checklist"]';     // CHOOSE ONLY ONE...
-    var SERVER_URL = 'https://co.audstanley.com/coffeeorders';    // if running on the shared server
+    //var SERVER_URL = 'https://co.audstanley.com/coffeeorders';    // if running on the shared server
+    var SERVER_URL = 'http://coffeerun-v2-rest-api.herokuapp.com/api/coffeeorders&retryWrites=false'; 
     //var SERVER_URL = 'http://localhost:3000/coffeeorders';          // if running locally
     var App = window.App;
     var Truck = App.Truck;
@@ -12,7 +13,8 @@
     var Validation = App.Validation;
     var CheckList = App.CheckList;
     var remoteDS = new RemoteDataStore(SERVER_URL);
-    // var truck = new Truck('ncc-1701', new DataStore());
+    //var remoteDS = new App.RemoteDataStore(SERVER_URL);
+    
     var truck = new Truck('ncc-1701', remoteDS);
     window.truck = truck;
     var checkList = new CheckList(CHECKLIST_SELECTOR);
